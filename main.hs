31c05652f18c57cb8a3186fefe02378ba0bbdee6
Eoin Davey <eoin.davey@arista.com>
2017-06-26 14:33:29 +0100

    Read line by line instead of by sentences, add trump text file

--- main.hs
+++ main.hs
@@ -4,33 +4,32 @@ import Data.Monoid
 import System.Random
 import Data.List.Split
 
-data Prefix = Prefix [String] deriving (Show, Eq, Ord)
-
-buildKeys :: [String] -> [String] -> Int-> [(Prefix, [String])]
-buildKeys [] x n = buildKeys (replicate n "") x n
+buildKeys :: (Monoid a, Ord a) => [a] -> [a] -> Int-> [([a], [a])]
+buildKeys [] x n = buildKeys (replicate n mempty) x n
 buildKeys _ [] _ = []
 buildKeys carry l@(x:xs) n
-    | (length l) > n = (Prefix carry, [x]) : (buildKeys ((tail carry) ++ [x]) xs n)
+    | (length l) > n = (carry, [x]) : (buildKeys ((tail carry) ++ [x]) xs n)
     | otherwise = []
 
-buildMap :: [String] -> Int -> Map.Map Prefix [String]
+buildMap :: (Monoid a, Ord a)=> [a] -> Int -> Map.Map [a] [a]
 buildMap x n = Map.fromListWith mappend $ buildKeys [] x n
 
-pickWord :: Prefix -> Map.Map Prefix [String] -> IO String
+pickWord :: (Monoid a, Ord a) => [a] -> Map.Map [a] [a] -> IO a
 pickWord p m = let as = g $ Map.lookup p m in randomRIO (0, (length as)-1) >>= (return . (as!!))
-    where g Nothing = [""]
+    where g Nothing = [mempty]
           g (Just as) = as
 
-gen :: Int -> Prefix -> Map.Map Prefix [String] -> IO [String]
+gen :: (Monoid a, Ord a) => Int -> [a] -> Map.Map [a] [a] -> IO [a]
 gen 0 _ _ = return []
-gen n p@(Prefix pa) m = do
-    nxt <- pickWord p m :: IO String
-    fol <- gen (n-1) (Prefix ((tail pa) ++ [nxt])) m
+gen n p m = do
+    nxt <- pickWord p m
+    fol <- gen (n-1) ((tail p) ++ [nxt]) m
     return (nxt:fol)
 
 main = do
     fullTxt <- getContents
-    let n = 3
-    let sentences = splitOn "." fullTxt :: [String]
+    let n = 2
+    let sentences = lines fullTxt :: [String]
     let mp = foldr (Map.unionWith (++)) Map.empty $ map (\x -> buildMap (words x) n) sentences
-    gen 60 (Prefix (replicate n "")) mp >>= (putStrLn . unwords)
+    gen 60 (replicate n "") mp >>= (putStrLn . unwords)
+    --print $ mp

b3458d1bef9b447af7cf0a79834cbc4c5eeb04ab
Eoin Davey <eoind@vey.ie>
2017-06-29 23:09:43 +0100

    Changed formatting

--- main.hs
+++ main.hs
@@ -4,22 +4,24 @@ import Control.Monad.Trans.Maybe
 import System.Random
 import qualified Twitter as T
 
-buildKeys :: [String] -> [String] -> Int-> [([String], [String])]
+type Prefix = [String]
+
+buildKeys :: Prefix -> [String] -> Int-> [(Prefix, [String])]
 buildKeys [] x n = buildKeys (replicate n mempty) x n
 buildKeys _ [] _ = []
 buildKeys carry l@(x:xs) n
     | (length l) > n = (carry, [x]) : (buildKeys ((tail carry) ++ [x]) xs n)
     | otherwise = []
 
-buildMap :: [String] -> Int -> Map.Map [String] [String]
+buildMap :: [String] -> Int -> Map.Map Prefix [String]
 buildMap x n = Map.fromListWith mappend $ buildKeys [] x n
 
-pickWord :: [String] -> Map.Map [String] [String] -> IO String
+pickWord :: Prefix -> Map.Map Prefix [String] -> IO String
 pickWord p m = let as = g $ Map.lookup p m in randomRIO (0, (length as)-1) >>= (return . (as!!))
     where g Nothing = [mempty]
           g (Just xs) = xs
 
-gen :: Int -> [String] -> Map.Map [String] [String] -> IO [String]
+gen :: Int -> Prefix -> Map.Map Prefix [String] -> IO [String]
 gen 0 _ _ = return []
 gen n p m = do
     nxt <- pickWord p m :: IO String
@@ -47,5 +49,5 @@ genTweetFromUser user_id = runMaybeT $ do
     let mp = foldr (Map.unionWith mappend) Map.empty $ map (\x -> buildMap (words x) n) sentences
     liftIO $ gen 20 (replicate n "") mp >>= (return . (foldr ignoreBlanks ""))
 
-main = genTweetFromUser "25073877" >>= \x -> case x of Just a -> print a
+main = genTweetFromUser "25073877" >>= \x -> case x of Just a -> T.tweet a
                                                        Nothing -> print "Error"
